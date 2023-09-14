5e97318f40f130bcf137979b61a01fb24a81d196
Gladwin Mohlamonyane <gladwin.mohlamonyane@mwrcybersec.com>
2023-09-14 16:44:58 +0200

    added more information to taffybar

--- .config/taffybar/taffybar.hs
+++ .config/taffybar/taffybar.hs
@@ -19,11 +19,29 @@ import System.Log.Logger
 import System.Taffybar.Information.EWMHDesktopInfo
 import System.Taffybar.Widget.SNITray
 import System.Taffybar.Information.CPU
+import System.Taffybar.Information.Memory
 import System.Taffybar.Widget.Weather
+import System.Taffybar.Widget.Text.NetworkMonitor
 
+cpuCallback = do
+  (_, systemLoad, totalLoad) <- cpuLoad
+  return [ totalLoad, systemLoad ]
+
+memCallback = do
+  mi <- parseMeminfo
+  return [memoryUsedRatio mi]
 
 main = do
   let wcfg = (defaultWeatherConfig "FAPR") { weatherTemplate = "Temp: $tempC$ 🌡️| Humidity: $humidity$" }
+  let cpuCfg = def { graphDataColors = [ (0, 1, 0, 1), (1, 0, 1, 0.5)]
+    , graphLabel = Just "cpu"
+    }
+  let memCfg = def { graphDataColors = [(0.8, 0.4, 0.4, 1)]
+    , graphLabel = Just "mem"
+    }
+  let netCfg = def { graphDataColors = [(0.9453125, 0.63671875, 0.2109375, 1.0), (0.9921875, 0.796875, 0.32421875, 1.0)]
+    , graphLabel = Just "net"
+    }
   --cssPath <- (</> "taffybar.css") <$> getDataDir
   let cssPath = "/home/gladwin/.config/taffybar/taffybar.css"
   print cssPath
@@ -51,6 +69,9 @@ main = do
       layout = layoutNew defaultLayoutConfig
       weatherWidget = weatherNew wcfg 10
       windows = windowsNew defaultWindowsConfig
+      cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
+      mem = pollingGraphNew memCfg 1 memCallback
+      net = networkGraphNew netCfg Nothing
       myMpris = mpris2New
           -- See https://github.com/taffybar/gtk-sni-tray#statusnotifierwatcher
           -- for a better way to set up the sni tray
@@ -62,6 +83,9 @@ main = do
             map (>>= buildContentsBox) [ windows, layout ]
         , endWidgets = map (>>= buildContentsBox)
           [ clock
+          , cpu
+          , mem
+          , net
           , tray
           , weatherWidget
           , myMpris
