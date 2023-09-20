2319dc9f4826e080632e4e9a2031022c3da5b65b
Gladwin Mohlamonyane <gladwin.mohlamonyane@mwrcybersec.com>
2023-09-20 13:30:28 +0200

    added pandoc to the exclusion list

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
-ignore=`checkupdates | grep -E "haskell|xmonad|xmobar|taffybar" | cut -d" " -f 1`
+ignore=`checkupdates | grep -E "haskell|pandoc|xmonad|xmobar|taffybar" | cut -d" " -f 1`
 packages=`echo -n $ignore | sed 's/\s\+/,/g'`
 yay -Syyu --ignore=$packages
