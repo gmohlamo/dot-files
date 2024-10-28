d8811aa44ac00b91d5f82d4b1317c4fad509daa8
Gladwin Tshepo Mohlamonyane <g.mohlamonyane@gmail.com>
2024-10-28 22:27:59 +0200

    forgot the keybinds

--- .config/nvim/lua/plugins/paste-images.lua
+++ .config/nvim/lua/plugins/paste-images.lua
@@ -1,10 +1,15 @@
 return {
 	"TobinPalmer/pastify.nvim",
+	cmd = { 'Pastify', 'PastifyAfter' },
+	event = { 'BufReadPost' },
+	keys = {
+		{ noremap = true, mode = "n", '<leader>P', "<cmd>Pastify<CR>" },
+	},
 	config = function()
 		require('pastify').setup({
 			opts = {
 				absolute_path = true,
-				local_path = os.getenv("HOME") .. "/Documents/Notes/vim.wiki/assets/",
+				local_path = "/assets/",
 				save = "local",
 				filename = '',
 				default_ft = 'markdown',
