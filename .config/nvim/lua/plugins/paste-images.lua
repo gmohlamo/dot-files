f603ce5d8147d681ef8a655384e9684f997aa610
Gladwin Tshepo Mohlamonyane <g.mohlamonyane@gmail.com>
2024-11-03 16:03:08 +0200

    Lazy load pastify on filetype

--- .config/nvim/lua/plugins/paste-images.lua
+++ .config/nvim/lua/plugins/paste-images.lua
@@ -1,15 +1,14 @@
 return {
 	"gmohlamo/pastify.nvim", --like the package... Just wanted to change how the "absolute" bit worked
 	cmd = { 'Pastify', 'PastifyAfter' },
-	event = { 'BufReadPost' },
+	event = { 'BufReadPost *.wiki' },
 	keys = {
-		{ noremap = true, mode = "n", '<leader>P', "<cmd>Pastify<CR>" },
+		{ noremap = true, mode = "n", '<leader>P', "<cmd>PastifyAfter<CR>" },
 	},
 	config = function()
 		require('pastify').setup({
 			opts = {
-				absolute_path = true,
-				local_path = "/Documents/Notes/vim.wiki/assets/",
+				local_path = "/imgs/",
 				save = "local",
 				filename = '',
 				default_ft = 'markdown',
