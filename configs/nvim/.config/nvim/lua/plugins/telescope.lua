ec7319c50a3615c4cd36e61df5c47ca594464012
Gladwin Tshepo Mohlamonyane <g.mohlamonyane@gmail.com>
2024-11-07 20:43:56 +0200

    got telescope to fuzzy search my notes

--- configs/nvim/.config/nvim/lua/plugins/telescope.lua
+++ configs/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,23 @@
 return {
 	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-telescope/telescope-live-grep-args.nvim",
+	},
 	config = function()
-		require('telescope').setup {}
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+		vim.keymap.set('n', '<leader>fs', require('telescope').extensions.live_grep_args.live_grep_args,
+			{ noremap = true, desc = 'Telescope live_grep_args in notes' })
+		require('telescope').setup({
+			extensions = {
+				live_grep_args = {
+					search_dirs = { "~/Documents/Notes/vim.wiki/" },
+				},
+			},
+		})
+		require('telescope').load_extension('live_grep_args')
 	end,
 }
